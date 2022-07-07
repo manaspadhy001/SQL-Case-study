7945b587f4c07441a0074b1f0ef7820a7ecd8c13
Manas Padhy <106370350+manaspadhy001@users.noreply.github.com>
2022-07-07 22:27:05 +0530

    Add files via upload

---
+++ IMDB question.sql
@@ -0,0 +1,347 @@
+USE imdb;
+
+/* 
+ To begin with, it is beneficial to know the shape of the tables and whether any column has null values.
+ Further we will take a look at 'movies' and 'genre' tables.*/
+
+
+
+-- Number of rows = 3867
+SELECT COUNT(*) FROM DIRECTOR_MAPPING;
+
+-- Number of rows = 14662
+SELECT COUNT(*) FROM GENRE ;
+
+-- Number of rows = 7997
+SELECT COUNT(*) FROM  MOVIE;
+
+-- Number of rows = 25735
+SELECT COUNT(*) FROM  NAMES;
+
+-- Number of rows = 7997
+SELECT COUNT(*) FROM  RATINGS;
+
+-- Number of rows = 15615
+SELECT COUNT(*) FROM  ROLE_MAPPING;
+
+
+
+SELECT Sum(CASE
+             WHEN id IS NULL THEN 1
+             ELSE 0
+           END) AS ID_NULL_COUNT,
+       Sum(CASE
+             WHEN title IS NULL THEN 1
+             ELSE 0
+           END) AS title_NULL_COUNT,
+       Sum(CASE
+             WHEN year IS NULL THEN 1
+             ELSE 0
+           END) AS year_NULL_COUNT,
+       Sum(CASE
+             WHEN date_published IS NULL THEN 1
+             ELSE 0
+           END) AS date_published_NULL_COUNT,
+       Sum(CASE
+             WHEN duration IS NULL THEN 1
+             ELSE 0
+           END) AS duration_NULL_COUNT,
+       Sum(CASE
+             WHEN country IS NULL THEN 1
+             ELSE 0
+           END) AS country_NULL_COUNT,
+       Sum(CASE
+             WHEN worlwide_gross_income IS NULL THEN 1
+             ELSE 0
+           END) AS worlwide_gross_income_NULL_COUNT,
+       Sum(CASE
+             WHEN languages IS NULL THEN 1
+             ELSE 0
+           END) AS languages_NULL_COUNT,
+       Sum(CASE
+             WHEN production_company IS NULL THEN 1
+             ELSE 0
+           END) AS production_company_NULL_COUNT
+FROM   movie; 
+
+SELECT year,
+       Count(title) AS NUMBER_OF_MOVIES
+FROM   movie
+GROUP  BY year;
+
+-- Number of movies released each month 
+SELECT Month(date_published) AS MONTH_NUM,
+       Count(*)              AS NUMBER_OF_MOVIES
+FROM   movie
+GROUP  BY month_num
+ORDER  BY month_num; 
+
+
+SELECT Count(DISTINCT id) AS number_of_movies, year
+FROM   movie
+WHERE  ( country LIKE '%INDIA%'
+          OR country LIKE '%USA%' )
+       AND year = 2019; 
+
+
+SELECT DISTINCT genre
+FROM   genre; 
+
+
+SELECT     genre,
+           Count(m.id) AS number_of_movies
+FROM       movie       AS m
+INNER JOIN genre       AS g
+where      g.movie_id = m.id
+GROUP BY   genre
+ORDER BY   number_of_movies DESC limit 1 ;
+
+
+WITH movies_with_one_genre
+     AS (SELECT movie_id
+         FROM   genre
+         GROUP  BY movie_id
+         HAVING Count(DISTINCT genre) = 1)
+SELECT Count(*) AS movies_with_one_genre
+FROM   movies_with_one_genre; 
+
+
+
+
+SELECT     genre,
+           Round(Avg(duration),2) AS avg_duration
+FROM       movie                  AS m
+INNER JOIN genre                  AS g
+ON      g.movie_id = m.id
+GROUP BY   genre
+ORDER BY avg_duration DESC;
+
+
+
+
+WITH genre_summary AS
+(
+           SELECT     genre,
+                      Count(movie_id)                            AS movie_count ,
+                      Rank() OVER(ORDER BY Count(movie_id) DESC) AS genre_rank
+           FROM       genre                                 
+           GROUP BY   genre )
+SELECT *
+FROM   genre_summary
+WHERE  genre = "THRILLER" ;
+
+-- 
+
+
+
+-- 
+SELECT Min(avg_rating)    AS MIN_AVG_RATING,
+       Max(avg_rating)    AS MAX_AVG_RATING,
+       Min(total_votes)   AS MIN_TOTAL_VOTES,
+       Max(total_votes)   AS MAX_TOTAL_VOTES,
+       Min(median_rating) AS MIN_MEDIAN_RATING,
+       Max(median_rating) AS MAX_MEDIAN_RATING
+FROM   ratings; 
+
+
+
+SELECT     title,
+           avg_rating,
+           Rank() OVER(ORDER BY avg_rating DESC) AS movie_rank
+FROM       ratings                               AS r
+INNER JOIN movie                                 AS m
+ON         m.id = r.movie_id limit 10;
+
+--
+WITH MOVIE_RANK AS
+(
+SELECT     title,
+           avg_rating,
+           ROW_NUMBER() OVER(ORDER BY avg_rating DESC) AS movie_rank
+FROM       ratings                               AS r
+INNER JOIN movie                                 AS m
+ON         m.id = r.movie_id
+)
+SELECT * FROM MOVIE_RANK
+WHERE movie_rank<=10;
+
+--
+-- Finding the number of movies based on median rating and sorting based on movie count.
+SELECT median_rating,
+       Count(movie_id) AS movie_count
+FROM   ratings
+GROUP  BY median_rating
+ORDER  BY movie_count DESC; 
+
+
+WITH production_company_hit_movie_summary
+     AS (SELECT production_company,
+                Count(movie_id)                     AS MOVIE_COUNT,
+                Rank()
+                  OVER(
+                    ORDER BY Count(movie_id) DESC ) AS PROD_COMPANY_RANK
+         FROM   ratings AS R
+                INNER JOIN movie AS M
+                        ON M.id = R.movie_id
+         WHERE  avg_rating > 8
+                AND production_company IS NOT NULL
+         GROUP  BY production_company)
+SELECT *
+FROM   production_company_hit_movie_summary
+WHERE  prod_company_rank = 1; 
+
+-- 
+
+SELECT genre,
+       Count(M.id) AS MOVIE_COUNT
+FROM   movie AS M
+       INNER JOIN genre AS G
+               ON G.movie_id = M.id
+       INNER JOIN ratings AS R
+               ON R.movie_id = M.id
+WHERE  year = 2017
+       AND Month(date_published) = 3
+       AND country LIKE '%USA%'
+       AND total_votes > 1000
+GROUP  BY genre
+ORDER  BY movie_count DESC; 
+
+--
+
+
+
+SELECT  title,
+       avg_rating,
+       genre
+FROM   movie AS M
+       INNER JOIN genre AS G
+               ON G.movie_id = M.id
+       INNER JOIN ratings AS R
+               ON R.movie_id = M.id
+WHERE  avg_rating > 8
+       AND title LIKE 'THE%'
+GROUP BY title
+ORDER BY avg_rating DESC;
+
+
+--
+
+-- BETWEEN operator is used to find the movies released between 1 April 2018 and 1 April 2019
+SELECT median_rating, Count(*) AS movie_count
+FROM   movie AS M
+       INNER JOIN ratings AS R
+               ON R.movie_id = M.id
+WHERE  median_rating = 8
+       AND date_published BETWEEN '2018-04-01' AND '2019-04-01'
+GROUP BY median_rating;
+
+-
+
+-- Compute the total number of votes for German and Italian movies.
+SELECT languages,
+       Sum(total_votes) AS VOTES
+FROM   movie AS M
+       INNER JOIN ratings AS R
+               ON R.movie_id = M.id
+WHERE  languages LIKE '%Italian%'
+UNION
+SELECT languages,
+       Sum(total_votes) AS VOTES
+FROM   movie AS M
+       INNER JOIN ratings AS R
+               ON R.movie_id = M.id
+WHERE  languages LIKE '%GERMAN%'
+ORDER  BY votes DESC; 
+
+--
+WITH VOTES_SUMMARY AS 
+(
+SELECT languages, SUM(total_votes) AS VOTES 
+FROM MOVIE AS M
+INNER JOIN RATINGS AS R 
+ON R.MOVIE_ID = M.ID
+WHERE languages like '%Italian%'
+UNION
+SELECT languages, SUM(total_votes) AS VOTES 
+FROM MOVIE AS M
+INNER JOIN RATINGS AS R 
+ON R.MOVIE_ID = M.ID
+WHERE languages like '%GERMAN%'
+),
+LANGUAGE_VOTE AS
+(
+SELECT languages FROM VOTES_SUMMARY
+ORDER BY VOTES DESC
+LIMIT 1)
+SELECT IF (languages LIKE 'GERMAN' , 'YES', 'NO') AS ANSWER
+FROM LANGUAGE_VOTE ;
+
+-- Approach 2: By country column
+SELECT country, sum(total_votes) as total_votes
+FROM movie AS m
+	INNER JOIN ratings as r ON m.id=r.movie_id
+WHERE country = 'Germany' or country = 'Italy'
+GROUP BY country;
+
+--
+
+
+
+
+
+SELECT Count(*) AS name_nulls
+FROM   names
+WHERE  NAME IS NULL;
+
+SELECT Count(*) AS height_nulls
+FROM   names
+WHERE  height IS NULL;
+
+SELECT Count(*) AS date_of_birth_nulls
+FROM   names
+WHERE  date_of_birth IS NULL;
+
+SELECT Count(*) AS known_for_movies_nulls
+FROM   names
+WHERE  known_for_movies IS NULL; 
+
+
+-- NULL counts for columns of names table using CASE statements
+SELECT 
+		SUM(CASE WHEN name IS NULL THEN 1 ELSE 0 END) AS name_nulls, 
+		SUM(CASE WHEN height IS NULL THEN 1 ELSE 0 END) AS height_nulls,
+		SUM(CASE WHEN date_of_birth IS NULL THEN 1 ELSE 0 END) AS date_of_birth_nulls,
+		SUM(CASE WHEN known_for_movies IS NULL THEN 1 ELSE 0 END) AS known_for_movies_nulls
+		
+FROM names;
+
+
+
+WITH top_3_genres AS
+(
+           SELECT     genre,
+                      Count(m.id)                            AS movie_count ,
+                      Rank() OVER(ORDER BY Count(m.id) DESC) AS genre_rank
+           FROM       movie                                  AS m
+           INNER JOIN genre                                  AS g
+           ON         g.movie_id = m.id
+           INNER JOIN ratings AS r
+           ON         r.movie_id = m.id
+           WHERE      avg_rating > 8
+           GROUP BY   genre limit 3 )
+SELECT     n.NAME            AS director_name ,
+           Count(d.movie_id) AS movie_count
+FROM       director_mapping  AS d
+INNER JOIN genre G
+using     (movie_id)
+INNER JOIN names AS n
+ON         n.id = d.name_id
+INNER JOIN top_3_genres
+using     (genre)
+INNER JOIN ratings
+using      (movie_id)
+WHERE      avg_rating > 8
+GROUP BY   NAME
+ORDER BY   movie_count DESC limit 3 ;
+
+
